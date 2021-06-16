bab2a2ecb312a37ab6f06f4881f0b934f79e4a4a
ducnq <nguyen.quang.duc@vsi-international.com>
2021-06-16 12:56:50 +0700

    deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC1J7uQcR1SvfTRF3btXhL3rTHwQ1EqzME",
+    authDomain: "blog-8d706.firebaseapp.com",
+    projectId: "blog-8d706",
+    storageBucket: "blog-8d706.appspot.com",
+    messagingSenderId: "412529844435",
+    appId: "1:412529844435:web:5d806431edaf172563b426",
+    measurementId: "G-F6XRDX6ZMM"
+  },
 };
