5e106e71abd624f59ef7941daa66a31e1e414ec3
ducnq <nguyen.quang.duc@vsi-international.com>
2021-06-16 12:08:46 +0700

    fix build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyC1J7uQcR1SvfTRF3btXhL3rTHwQ1EqzME",
     authDomain: "blog-8d706.firebaseapp.com",
